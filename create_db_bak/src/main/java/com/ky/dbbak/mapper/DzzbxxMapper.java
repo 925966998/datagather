4829568006822a253c786d1f3fcd2c1a036034bf
caozewang <loverenzhiQianer>
2020-03-23 15:16:18 +0800

    三张表数据完善,批量插入

--- create_db_bak/src/main/java/com/ky/dbbak/mapper/DzzbxxMapper.java
+++ create_db_bak/src/main/java/com/ky/dbbak/mapper/DzzbxxMapper.java
@@ -1,7 +1,7 @@
 package com.ky.dbbak.mapper;
 
 import com.ky.dbbak.mybatis.BaseMapper;
-import org.apache.ibatis.annotations.InsertProvider;
+import com.ky.dbbak.targetmapper.DzzbxxSql;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.SelectProvider;
 
