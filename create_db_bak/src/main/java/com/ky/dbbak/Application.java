6d3c95abba877569f59d5e538fa31e0301846f63
linan <925966998@qq.com>
2020-03-25 09:08:31 +0800

    ==

--- create_db_bak/src/main/java/com/ky/dbbak/Application.java
+++ create_db_bak/src/main/java/com/ky/dbbak/Application.java
@@ -11,10 +11,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
 
--- create_db_bak/src/main/java/com/ky/dbbak/controller/SysUserController.java
+++ create_db_bak/src/main/java/com/ky/dbbak/controller/SysUserController.java
@@ -29,7 +29,6 @@ public class SysUserController {
     @Autowired
     SysUserService sysUserService;
 
-
     @Autowired
     SysUserMapper sysUserMapper;
 
