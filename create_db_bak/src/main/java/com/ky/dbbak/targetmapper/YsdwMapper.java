7d05538109276b5a0690b24c2ae65d4b1624b33b
linan <925966998@qq.com>
2020-03-29 21:54:30 +0800

    1

--- create_db_bak/src/main/java/com/ky/dbbak/targetmapper/YsdwMapper.java
+++ create_db_bak/src/main/java/com/ky/dbbak/targetmapper/YsdwMapper.java
@@ -1,14 +1,13 @@
 package com.ky.dbbak.targetmapper;
 
-import com.ky.dbbak.entity.AreaEntity;
 import com.ky.dbbak.mybatis.BaseMapper;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface YsdwMapper extends BaseMapper {
 
 
-    @Select("delete from dbo.YSDW where DWDM=#{dwdm} and DWMC=#{dwmc} and XZQHDM=#{xzqhdm}")
-    AreaEntity deleteYsdw(String dwdm, String dwmc, String xzqhdm);
+    @Delete("delete from dbo.YSDW where DWDM=#{dwdm} and DWMC=#{dwmc} and XZQHDM=#{xzqhdm}")
+    void deleteYsdw(String dwdm, String dwmc, String xzqhdm);
 }
