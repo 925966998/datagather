67b90ddfb9cf017237a64007a7ecd54783828a52
yaoweijie <yaoweijie950809@163.com>
2020-03-30 19:45:13 +0800

    科目年初+

--- create_db_bak/src/main/java/com/ky/dbbak/targetmapper/KMNCSSql.java
+++ create_db_bak/src/main/java/com/ky/dbbak/targetmapper/KMNCSSql.java
@@ -141,4 +141,93 @@ public class KMNCSSql extends BaseProvider {
         return builder.toString();
     }
 
+    public String _updateKmncs(Map map) {
+        StringBuilder builder = new StringBuilder();
+        StringBuilder builder1 = new StringBuilder();
+        for (String c : getColumns()) {
+            if (map.get(c) != null) {
+                builder1.append(",").append(c).append("=#{" + c + "}");
+            }
+        }
+        builder.append("update ").append(getTableName());
+        builder.append(" set ");
+        builder.append(builder1.substring(1, builder1.toString().length()));
+        builder.append(" where KJDZZBBH=#{KJDZZBBH}");
+        builder.append(" and KJYF=#{KJYF}");
+        builder.append(" and KJKMBM=#{KJKMBM}");
+        return builder.toString();
+    }
+
+
+    public String querySum(Map map) {
+        StringBuilder builder = new StringBuilder("select * from " + getTableName() + " where 1=1");
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJYF"))) {
+            builder.append(" and KJYF=#{KJYF}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJKMBM"))) {
+            builder.append(" and KJKMBM=#{KJKMBM}");
+        }
+
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "XZQHDM"))) {
+            builder.append(" and XZQHDM=#{XZQHDM}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "XZQHMC"))) {
+            builder.append(" and XZQHMC=#{XZQHMC}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJND"))) {
+            builder.append(" and KJND=#{KJND}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "DWMC"))) {
+            builder.append(" and DWMC=#{DWMC}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "DWDM"))) {
+            builder.append(" and DWDM=#{DWDM}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJDZZBBH"))) {
+            builder.append(" and KJDZZBBH=#{KJDZZBBH}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJDZZBMC"))) {
+            builder.append(" and KJDZZBMC=#{KJDZZBMC}");
+        }
+
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJTX"))) {
+            builder.append(" and KJTX=#{KJTX}");
+        }
+
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJKMMC"))) {
+            builder.append(" and KJKMMC=#{KJKMMC}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KMQC"))) {
+            builder.append(" and KMQC=#{KMQC}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "KJKMJC"))) {
+            builder.append(" and KJKMJC=#{KJKMJC}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "SFZDJKM"))) {
+            builder.append(" and SFZDJKM=#{SFZDJKM}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "SJKMBM"))) {
+            builder.append(" and SJKMBM=#{SJKMBM}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "SFXJHXJDJW"))) {
+            builder.append(" and SFXJHXJDJW=#{SFXJHXJDJW}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "BZMC"))) {
+            builder.append(" and BZMC=#{BZMC}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "YEFX"))) {
+            builder.append(" and YEFX=#{YEFX}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "BBQCYE"))) {
+            builder.append(" and BBQCYE=#{BBQCYE}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "QCSL"))) {
+            builder.append(" and QCSL=#{QCSL}");
+        }
+        if (StringUtils.isNotEmpty(MapUtils.getString(map, "WBQCYE"))) {
+            builder.append(" and WBQCYE=#{WBQCYE}");
+        }
+        return builder.toString();
+    }
+
 }
