32dd3d8e738e88fe61359329091f1e213f1c1a2d
linan <925966998@qq.com>
2020-04-15 10:35:26 +0800

    1

--- red-wood/src/main/java/com/ky/redwood/controller/HomeController.java
+++ red-wood/src/main/java/com/ky/redwood/controller/HomeController.java
@@ -3,18 +3,14 @@ package com.ky.redwood.controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class HomeController extends WebMvcConfigurerAdapter {
+public class HomeController implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-
         registry.addViewController("/").setViewName("forward:/login.html");
-
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        super.addViewControllers(registry);
-
     }
 }
