6e2a3786fb8b5749f14b6be242030b06e9c9b6b2
925966998 <loverenzhiQianer>
2020-05-13 17:02:02 +0800

    "1111"

--- ykt/src/main/java/com/ky/ykt/interceptor/DeleteFileSchedul.java
+++ ykt/src/main/java/com/ky/ykt/interceptor/DeleteFileSchedul.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @ClassName DeleteFileSchedul
@@ -23,9 +24,53 @@ public class DeleteFileSchedul {
     //每天00：00执行
     @Scheduled(cron = "0 00 00 ? * *")
     public void deleteFile() {
-        String property = System.getProperty("user.dir");
+        /*String property = System.getProperty("user.dir");
         String path = property + "**********\\20200429170147.xls";
         File file = new File(path);
-        file.delete();
+        file.delete();*/
+        String file = "D:/1111" ;
+        delFolder(file);
+    }
+    public static void delFolder(String folderPath) {
+        try {
+            delAllFile(folderPath); // 删除完里面所有内容
+            //不想删除文佳夹隐藏下面
+//			String filePath = folderPath;
+//			filePath = filePath.toString();
+//			java.io.File myFilePath = new java.io.File(filePath);
+//			myFilePath.delete(); // 删除空文件夹
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean delAllFile(String path) {
+        boolean flag = false;
+        File file = new File(path);
+        System.out.println(path);
+        if (!file.exists()) {
+            return flag;
+        }
+        if (!file.isDirectory()) {
+            return flag;
+        }
+        String[] tempList = file.list();
+        File temp = null;
+        System.out.println(tempList.length);
+        for (int i = 0; i < tempList.length; i++) {
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + tempList[i]);
+            } else {
+                temp = new File(path + File.separator + tempList[i]);
+            }
+            if (temp.isFile()) {
+                temp.delete();
+            }
+            if (temp.isDirectory()) {
+                delAllFile(path + "/" + tempList[i]);// 先删除文件夹里面的文件
+//				delFolder(path + "/" + tempList[i]);// 再删除空文件夹
+                flag = true;
+            }
+        }
+        return flag;
     }
 }
