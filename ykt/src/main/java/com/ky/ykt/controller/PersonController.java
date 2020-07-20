8f1c017d2aee3dd70e928450d8561bb6020ea753
yaoWeiJie <yaoweijie950809@163.com>
2020-07-20 15:35:32 +0800

    社保卡号变银行卡号

--- ykt/src/main/java/com/ky/ykt/controller/PersonController.java
+++ ykt/src/main/java/com/ky/ykt/controller/PersonController.java
@@ -358,7 +358,7 @@ public class PersonController {
             });
         }
         resultMap.put("header",
-                new String[]{"姓名", "手机号", "身份证号", "社保卡号", "项目资金名称", "发放金额", "所属区县", "所属乡镇", "详细地址", "流水号", "回执状态"});
+                new String[]{"姓名", "手机号", "身份证号", "银行卡号", "项目资金名称", "发放金额", "所属区县", "所属乡镇", "详细地址", "流水号", "回执状态"});
         resultMap.put("data", data);
         resultMap.put("style", style);
         return resultMap;
