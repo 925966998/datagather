9dcb6f707aae28076c3a30d91be18875df378714
yaoWeiJie <yaoweijie950809@163.com>
2021-02-03 11:31:06 +0800

    1

--- create_db_bak/src/main/java/com/ky/dbbak/mapper/PzfzmxSql.java
+++ create_db_bak/src/main/java/com/ky/dbbak/mapper/PzfzmxSql.java
@@ -16,7 +16,7 @@ public class PzfzmxSql extends BaseProvider {
     @Override
     protected String[] getColumns() {
         return new String[]{
-                "KJYF", "JZPZBH", "FLXH", "XZQHDM", "XZQHMC", "KJND", "DWMC", "DWDM", "KJDZZBBH", "KJDZZBMC", "PZLXBH", "JZPZZL", "JZPZHH", "JZPZZY", "KJTX", "KJKMBM", "KJKMMC", "FZLX", "FZBM", "FZMC", "FZQC", "YJFZBM", "EJFZBM", "SJFZBM", "SIJFZBM", "WJFZBM", "JFFSE", "DFFSE", "BZ", "WBJFFSE", "WBDFFSE", "HL", "SL", "DJ"
+                "KJYF", "JZPZBH", "FLXH", "XZQHDM", "XZQHMC", "KJND", "DWMC", "DWDM", "KJDZZBBH", "KJDZZBMC", "PZLXBH", "JZPZZL", "JZPZHH", "JZPZZY", "KJTX", "KJKMBM", "KJKMMC", "FZLX", "FZBM", "FZMC", "FZQC", "YJFZBM", "EJFZBM", "SJFZBM", "SIJFZBM", "WJFZBM", "JFFSE", "DFFSE", "BZ", "WBJFFSE", "WBDFFSE", "HL", "SL", "DJ","JZLXMC"
         };
     }
 
